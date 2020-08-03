0ed53ab9fc5ead9ec2b3e218227276c743786dd9
Sankararao Kruthiventi <Sankararao.Kruthiventi@OAKNORTH.AI>
2020-08-03 20:59:06 +0530

    home view update

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,51 +1,138 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./home.style.css";
-
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import Scrollbar from "react-smooth-scrollbar";
 
-let top100Films = [
-  /* { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 }, */
-];
-
 const Home = () => {
-  const fetchOptions = (event, value) => {
-    console.log(value);
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=fceee37c892707ca488b3969171ef2b9&language=en-US&query=${value}&page=1`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        let movies = [];
-        data.results.map((movie) => {
-          return movies.push({ title: movie.original_title, id: movie.id });
-        });
-        console.log(movies);
-        top100Films = movies;
+  const [display, setDisplay] = useState(false);
+  let [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    /* const promises = new Array(1)
+      .fill()
+      .map((value, i) =>
+        fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=fceee37c892707ca488b3969171ef2b9&language=en-US&query=${value}&page=1`
+        )
+      );
+    Promise.all(promises).then((pokemonArr) => {
+      return pokemonArr.map((value) => {
+        value
+          .json()
+          /* .then(({ name, sprites: { front_default: sprite } }) =>
+            pokemon.push({ name, sprite })
+          //) 
+          
+          .then(({ results }) => pokemon.push(...results));
+          setOptions(pokemon);
       });
+    }); */
+  });
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = (event) => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const updatePokeDex = (poke) => {
+    setSearch(poke);
+    setDisplay(false);
+  };
+
+  const searchMovie = (event) => {
+    console.log(event.target.value);
+    options = [];
+    setPokemon([]);
+    const promises = new Array(1)
+      .fill()
+      .map((value, i) =>
+        fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=fceee37c892707ca488b3969171ef2b9&language=en-US&query=${event.target.value}&page=1`
+        )
+      );
+    Promise.all(promises).then((pokemonArr) => {
+      return pokemonArr.map((value, index) => {
+        value
+          .json()
+          .then(({ results }) => {
+            if (results && results.length !== 0) {
+              pokemon.push(...results);
+              setOptions(pokemon);
+            }
+          });
+      });
+    });
   };
 
   return (
     <Scrollbar>
       <div className="home-contianer">
         <div className="finder-container">
-          <Autocomplete
-            id="movie-name"
-            freeSolo
-            options={top100Films}
-            getOptionLabel={(option) => option.title}
-            //   style={{ width: 300 }}
-            className="autocomplete"
-            variant="outlined" 
-            onInputChange={fetchOptions}
-            renderInput={(params) => (
-              <TextField {...params} label="Type to Search Movies" />
-            )}
-          />
+          <div className="finder">
+            <div className="find-text">Search for movies</div>
+
+            <div
+              ref={wrapperRef}
+              className="flex-container flex-column pos-rel"
+            >
+              <input
+                id="auto"
+                onClick={() => setDisplay(!display)}
+                placeholder="Type to search"
+                value={search}
+                autocomplete="off"
+                onChange={(event) => {
+                  setSearch(event.target.value);
+                }}
+                onKeyUp={searchMovie}
+              />
+              {display && options &&
+                      options.length && (
+                <div className="autoContainer">
+                  {
+                      options.map((movie, index) => {
+                        return (
+                          <div>
+                            {index < 3 && (
+                              <div
+                                // onClick={() => updatePokeDex(value.name)}
+                                className="option"
+                                key={index}
+                                tabIndex="0"
+                              >
+                                <span>{movie.original_title}</span>
+                                <div className="movie-search-poster-container">
+                                  <img
+                                    className="movie-search-poster"
+                                    src={
+                                      "https://image.tmdb.org/t/p/w500" +
+                                      movie.poster_path
+                                    }
+                                    alt="poster"
+                                  />
+                                </div>
+                              </div>
+                            )}
+                          </div>
+                        );
+                      })
+                  }
+                </div>
+              )}
+            </div>
+          </div>
         </div>
         <div className="home-view-container">
           <div className="about-container">
@@ -66,8 +153,8 @@ const Home = () => {
           <div className="about-container">
             <div className="about">About me</div>
             <div>
-              This app is developed by <b>ShankarKruthiventi</b>. You can find
-              reach out to me at
+              This app is developed by <b>ShankarKruthiventi</b>. You can reach
+              out to me at
             </div>
             <ul>
               <li>
